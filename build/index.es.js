c36403201ef8fbede83b231f77e831fc9b694c53
idrisakmal <simple_idris@yahoo.com>
2020-05-03 21:49:55 +0800

    Push build folder

---
+++ build/index.es.js
@@ -0,0 +1,566 @@
+import React, { forwardRef, useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import lottie from 'lottie-web';
+
+function _defineProperty(obj, key, value) {
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    });
+  } else {
+    obj[key] = value;
+  }
+
+  return obj;
+}
+
+function ownKeys(object, enumerableOnly) {
+  var keys = Object.keys(object);
+
+  if (Object.getOwnPropertySymbols) {
+    var symbols = Object.getOwnPropertySymbols(object);
+    if (enumerableOnly) symbols = symbols.filter(function (sym) {
+      return Object.getOwnPropertyDescriptor(object, sym).enumerable;
+    });
+    keys.push.apply(keys, symbols);
+  }
+
+  return keys;
+}
+
+function _objectSpread2(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i] != null ? arguments[i] : {};
+
+    if (i % 2) {
+      ownKeys(Object(source), true).forEach(function (key) {
+        _defineProperty(target, key, source[key]);
+      });
+    } else if (Object.getOwnPropertyDescriptors) {
+      Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
+    } else {
+      ownKeys(Object(source)).forEach(function (key) {
+        Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));
+      });
+    }
+  }
+
+  return target;
+}
+
+var Animator = forwardRef(function (props, ref) {
+  var animationData = props.animationData,
+      loop = props.loop,
+      autoplay = props.autoplay,
+      initialSegment = props.initialSegment,
+      onComplete = props.onComplete,
+      onLoopComplete = props.onLoopComplete,
+      onEnterFrame = props.onEnterFrame,
+      onSegmentStart = props.onSegmentStart,
+      onConfigReady = props.onConfigReady,
+      onDataReady = props.onDataReady,
+      onDataFailed = props.onDataFailed,
+      onLoadedImages = props.onLoadedImages,
+      onDOMLoaded = props.onDOMLoaded,
+      onDestroy = props.onDestroy,
+      style = props.style,
+      className = props.className;
+  var animationContainer = useRef(null);
+  var animationInstanceRef = useRef(null);
+  var parentRef = ref || useRef();
+  /*
+  	======================================
+  		INTERACTION METHODS
+  	======================================
+   */
+
+  /**
+   * Play
+   * TODO: complete
+   */
+
+  var play = function play() {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.play();
+    }
+  };
+  /**
+   * Stop
+   * TODO: complete
+   */
+
+
+  var stop = function stop() {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.stop();
+    }
+  };
+  /**
+   * Pause
+   * TODO: complete
+   */
+
+
+  var pause = function pause() {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.pause();
+    }
+  };
+  /**
+   * Set animation speed
+   * TODO: complete
+   * @param speed
+   */
+
+
+  var setSpeed = function setSpeed(speed) {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.setSpeed(speed);
+    }
+  };
+  /**
+   * Got to frame and play
+   * TODO: complete
+   * @param value
+   * @param isFrame
+   */
+
+
+  var goToAndPlay = function goToAndPlay(value, isFrame) {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.goToAndPlay(value, isFrame);
+    }
+  };
+  /**
+   * Got to frame and stop
+   * TODO: complete
+   * @param value
+   * @param isFrame
+   */
+
+
+  var goToAndStop = function goToAndStop(value, isFrame) {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.goToAndStop(value, isFrame);
+    }
+  };
+  /**
+   * Set animation direction
+   * TODO: complete
+   * @param direction
+   */
+
+
+  var setDirection = function setDirection(direction) {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.setDirection(direction);
+    }
+  };
+  /**
+   * Play animation segments
+   * TODO: complete
+   * @param segment
+   * @param force
+   */
+
+
+  var playSegments = function playSegments(segment, force) {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.playSegments(segment, force);
+    }
+  };
+  /**
+   * Set sub frames
+   * TODO: complete
+   * @param useSubFrames
+   */
+
+
+  var setSubframe = function setSubframe(useSubFrames) {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.setSubframe(useSubFrames);
+    }
+  };
+  /**
+   * Destroy animation
+   * TODO: complete
+   */
+
+
+  var destroy = function destroy() {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.destroy();
+    }
+  };
+  /**
+   * Get animation duration
+   * TODO: complete
+   * @param inFrames
+   */
+
+
+  var getDuration = function getDuration(inFrames) {
+    if (animationInstanceRef.current) {
+      return animationInstanceRef.current.getDuration(inFrames);
+    }
+
+    return undefined;
+  };
+  /*
+  	======================================
+  		LOTTIE
+  	======================================
+   */
+
+  /**
+   * Load a new animation, and if it's the case, destroy the previous one
+   * @param {Object} forceOptions
+   */
+
+
+  var loadAnimation = function loadAnimation() {
+    var forceOptions = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.destroy();
+    }
+
+    var config = _objectSpread2(_objectSpread2({
+      animationData: animationData || null,
+      loop: !Number.isNaN(loop) ? loop : loop !== false,
+      autoplay: autoplay !== false,
+      initialSegment: initialSegment || null
+    }, forceOptions), {}, {
+      container: animationContainer.current
+    });
+
+    animationInstanceRef.current = lottie.loadAnimation(config); // Share methods which control Lottie to the parent component
+
+    if (parentRef) {
+      parentRef.current = {
+        play: play,
+        stop: stop,
+        pause: pause,
+        setSpeed: setSpeed,
+        goToAndPlay: goToAndPlay,
+        goToAndStop: goToAndStop,
+        setDirection: setDirection,
+        playSegments: playSegments,
+        setSubframe: setSubframe,
+        destroy: destroy,
+        getDuration: getDuration
+      };
+    }
+  }; // Initialize and listen for changes that need to reinitialize Lottie
+
+
+  useEffect(function () {
+    loadAnimation();
+  }, [animationData, loop, autoplay, initialSegment]);
+  /*
+  	======================================
+  		EVENTS
+  	======================================
+   */
+
+  /**
+   * Handle the process of adding an event listener
+   * @param {String} eventName
+   * @param {Function} eventHandler
+   * @param {Boolean} removePreviousListeners
+   * @return {Function} Function that deregister the listener
+   */
+
+  var addEventListenerHelper = function addEventListenerHelper(eventName, eventHandler) {
+    var removePreviousListeners = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+
+    if (animationInstanceRef.current) {
+      if (removePreviousListeners) {
+        animationInstanceRef.current.removeEventListener(eventName);
+      }
+
+      if (eventName && eventHandler) {
+        animationInstanceRef.current.addEventListener(eventName, eventHandler); // Return a function to deregister the event
+
+        return function () {
+          // TODO: Should we remove all the listeners?
+          animationInstanceRef.current.removeEventListener(eventName, eventHandler);
+        };
+      }
+    }
+
+    return function () {};
+  };
+
+  useEffect(function () {
+    var listeners = [{
+      name: "complete",
+      handler: onComplete
+    }, {
+      name: "loopComplete",
+      handler: onLoopComplete
+    }, {
+      name: "enterFrame",
+      handler: onEnterFrame
+    }, {
+      name: "segmentStart",
+      handler: onSegmentStart
+    }, {
+      name: "config_ready",
+      handler: onConfigReady
+    }, {
+      name: "data_ready",
+      handler: onDataReady
+    }, {
+      name: "data_failed",
+      handler: onDataFailed
+    }, {
+      name: "loaded_images",
+      handler: onLoadedImages
+    }, {
+      name: "DOMLoaded",
+      handler: onDOMLoaded
+    }, {
+      name: "destroy",
+      handler: onDestroy
+    }];
+    var deregisterList = listeners.map(function (event) {
+      return addEventListenerHelper(event.name, event.handler);
+    }); // Deregister events on unmount
+
+    return function () {
+      deregisterList.forEach(function (deregister) {
+        return deregister();
+      });
+    };
+  }, [onComplete, onLoopComplete, onEnterFrame, onSegmentStart, onConfigReady, onDataReady, onDataFailed, onLoadedImages, onDOMLoaded, onDestroy]);
+  /**
+   * ALPHA
+   */
+  // Detect changes of the loop param and change it without reloading the animation
+  // TODO: needs intensive testing
+  // useEffect(() => {
+  // 	if (animationInstanceRef.current.loop !== loop) {
+  // 		animationInstanceRef.current.loop = loop;
+  // 	}
+  //
+  // 	// // TODO: decide if this is a desired behavior
+  // 	// if (animationInstanceRef.current.isPaused) {
+  // 	// 	animationInstanceRef.current.play();
+  // 	// }
+  // }, [loop]);
+
+  return /*#__PURE__*/React.createElement("div", {
+    ref: animationContainer,
+    style: style,
+    className: className
+  });
+});
+Animator.propTypes = {
+  animationData: PropTypes.shape(undefined).isRequired,
+  loop: PropTypes.oneOfType([PropTypes.bool, PropTypes.number]),
+  autoplay: PropTypes.bool,
+  initialSegment: PropTypes.arrayOf(PropTypes.shape(PropTypes.number.isRequired)),
+  onComplete: PropTypes.func,
+  onLoopComplete: PropTypes.func,
+  onEnterFrame: PropTypes.func,
+  onSegmentStart: PropTypes.func,
+  onConfigReady: PropTypes.func,
+  onDataReady: PropTypes.func,
+  onDataFailed: PropTypes.func,
+  onLoadedImages: PropTypes.func,
+  onDOMLoaded: PropTypes.func,
+  onDestroy: PropTypes.func,
+  style: PropTypes.shape(undefined)
+};
+Animator.defaultProps = {
+  loop: true,
+  autoplay: true,
+  initialSegment: null,
+  onComplete: null,
+  onLoopComplete: null,
+  onEnterFrame: null,
+  onSegmentStart: null,
+  onConfigReady: null,
+  onDataReady: null,
+  onDataFailed: null,
+  onLoadedImages: null,
+  onDOMLoaded: null,
+  onDestroy: null,
+  style: null
+};
+
+var useAnimator = function useAnimator(props) {
+  var style = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+  var animationInstanceRef = useRef(null);
+  var animationContainer = useRef(null); // Initialize Lottie
+
+  useEffect(function () {
+    animationInstanceRef.current = lottie.loadAnimation(_objectSpread2(_objectSpread2({}, props), {}, {
+      container: animationContainer.current
+    })); // eslint-disable-next-line
+  }, []);
+  /**
+   * Play
+   * TODO: complete
+   */
+
+  var play = function play() {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.play();
+    }
+  };
+  /**
+   * Stop
+   * TODO: complete
+   */
+
+
+  var stop = function stop() {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.stop();
+    }
+  };
+  /**
+   * Pause
+   * TODO: complete
+   */
+
+
+  var pause = function pause() {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.pause();
+    }
+  };
+  /**
+   * Set animation speed
+   * TODO: complete
+   * @param speed
+   */
+
+
+  var setSpeed = function setSpeed(speed) {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.setSpeed(speed);
+    }
+  };
+  /**
+   * Got to frame and stop
+   * TODO: complete
+   * @param value
+   * @param isFrame
+   */
+
+
+  var goToAndStop = function goToAndStop(value, isFrame) {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.goToAndStop(value, isFrame);
+    }
+  };
+  /**
+   * Got to frame and play
+   * TODO: complete
+   * @param value
+   * @param isFrame
+   */
+
+
+  var goToAndPlay = function goToAndPlay(value, isFrame) {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.goToAndPlay(value, isFrame);
+    }
+  };
+  /**
+   * Set animation direction
+   * TODO: complete
+   * @param direction
+   */
+
+
+  var setDirection = function setDirection(direction) {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.setDirection(direction);
+    }
+  };
+  /**
+   * Play animation segments
+   * TODO: complete
+   * @param segment
+   * @param force
+   */
+
+
+  var playSegments = function playSegments(segment, force) {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.playSegments(segment, force);
+    }
+  };
+  /**
+   * Set sub frames
+   * TODO: complete
+   * @param useSubFrames
+   */
+
+
+  var setSubframe = function setSubframe(useSubFrames) {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.setSubframe(useSubFrames);
+    }
+  };
+  /**
+   * Destroy animation
+   * TODO: complete
+   */
+
+
+  var destroy = function destroy() {
+    if (animationInstanceRef.current) {
+      animationInstanceRef.current.destroy();
+    }
+  };
+  /**
+   * Get animation duration
+   * TODO: complete
+   * @param inFrames
+   */
+
+
+  var getDuration = function getDuration(inFrames) {
+    if (animationInstanceRef.current) {
+      return animationInstanceRef.current.getDuration(inFrames);
+    }
+
+    return undefined;
+  }; // Build the animation view
+
+
+  var View = /*#__PURE__*/React.createElement("div", {
+    style: style,
+    ref: animationContainer
+  });
+  return {
+    View: View,
+    play: play,
+    stop: stop,
+    pause: pause,
+    setSpeed: setSpeed,
+    goToAndStop: goToAndStop,
+    goToAndPlay: goToAndPlay,
+    setDirection: setDirection,
+    playSegments: playSegments,
+    setSubframe: setSubframe,
+    destroy: destroy,
+    getDuration: getDuration
+  };
+};
+
+var index = {
+  Animator: Animator,
+  useAnimator: useAnimator
+};
+
+export default index;
+//# sourceMappingURL=index.es.js.map
